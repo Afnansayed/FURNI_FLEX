bb5ae79507ce536ce78b0cd58f7c9a8baa1ebd6c
Afnan Sayed <afnansayed1973@gimal.com>
2024-09-07 23:42:27 +0600

    AuthProvider

--- src/Shared/Signup/Signup.jsx
+++ src/Shared/Signup/Signup.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AuthForm from '../AuthForm/AuthForm';
+import { AuthContext } from '../../Providers/AuthProvider/AuthProvider';
 
 const Signup = () => {
     const [userData, setUserData] = useState(null);
+    const {user} = useContext(AuthContext)
     const handleFormSubmit = (data) => {
         setUserData(data);
         console.log('Form Data from Child:', data);
         // You can also send the formData to an API here
       };
-      console.log(userData)
+      console.log(user)
     return (
         <div>
              <AuthForm isSignUp={true} handleFormSubmit={handleFormSubmit}/>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Router/Router.jsx'
+import AuthProvider from './Providers/AuthProvider/AuthProvider.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <div className='max-w-[1420px] mx-auto'>
-           <RouterProvider router={router} />
+         <RouterProvider router={router} /> 
     </div>
+    </AuthProvider>
   </StrictMode>,
 )
